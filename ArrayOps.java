91b99879dc58c64a84e474ac6e93c45f8f00fed8
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2020-10-26 15:49:33 -0400

    I have completed largestInRows, sum for a 2d array, sumCols, isRowMagic, and isColMagic

--- ArrayOps.java
+++ ArrayOps.java
@@ -15,5 +15,56 @@ public class ArrayOps {
     }
     return highest;
   }
+  public static int[] sumRows(int[][] matrix) {
+    int[] arr = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      arr[i] = sum(matrix[i]);
+    }
+    return arr;
+  }
+  public static int[] largestInRows(int[][] matrix) {
+    int[] arr = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      arr[i] = largest(matrix[i]);
+    }
+    return arr;
+  }
+  public static int sum(int[][] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += sum(arr[i]);
+    }
+    return sum;
+  }
+  public static int[] sumCols(int[][] matrix) {
+    int[] arr = new int[matrix[0].length];
+    int sum = 0;
+    for (int i = 0; i < matrix[0].length; i++) {
+      sum = 0;
+      for (int j = 0; j < matrix.length; j++) {
+        sum += matrix[j][i];
+      }
+      arr[i] = sum;
+    }
+    return arr;
+  }
+  public static boolean isRowMagic(int[][] matrix) {
+    int[] sum = sumRows(matrix);
+    for (int i = 1; i < matrix.length; i++) {
+      if (sum[0] != sum[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  public static boolean isColMagic(int[][] matrix) {
+    int[] sum = sumCols(matrix);
+    for (int i = 1; i < matrix.length; i++) {
+      if (sum[0] != sum[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
-    int[] arr = {1, 5, 10, 3, 6, 1};
-    //System.out.println(ArrayOps.sum(arr));
-    System.out.println(ArrayOps.largest(arr));
+    int[][] A = {{1, 0, 12, -1}, {7, -2, 2, 1}, {-5, -2, 2, -9}};
+    int[] B = {1, 3, 5};
+    int[][] arr1 = {{1, 2, 3, 4}, {2, 3, 4, 1}, {3, 4, 1, 2}};
+    int[][] arr2 = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+    int[][] arr3 = {{2, 2, 2}, {2, 2, 2}};
+    System.out.println(ArrayOps.sum(B));
+    System.out.println(ArrayOps.largest(B));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+    System.out.println(ArrayOps.sum(A));
+    System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
+    System.out.println(ArrayOps.isRowMagic(arr1));
+    System.out.println(ArrayOps.isRowMagic(arr2));
+    System.out.println(ArrayOps.isRowMagic(arr3));
+    System.out.println(ArrayOps.isColMagic(arr1));
+    System.out.println(ArrayOps.isColMagic(arr2));
+    System.out.println(ArrayOps.isColMagic(arr3));
   }
 }
